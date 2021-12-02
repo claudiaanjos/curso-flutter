f7c9ea77d7ee4dfb905738d112b72c694490be92
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-02 20:04:04 -0300

    Switch

---
+++ Dart-Basico/switch.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  String linguagem = 'Dart';
+  
+  switch(linguagem) {
+    case 'C':
+      print("linguagem complicada");
+      break;
+    case 'Dart':
+      print("linguagem legal");
+      break;
+    case 'Python':
+      print("linguagem ótima");
+      break;
+    default:
+      print("Desconhecida");
+  }
+}
