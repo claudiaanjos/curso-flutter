fe00f26e3a392ebec56c5274f64ec81686f6c1d9
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-02 19:30:34 -0300

    Operadores Lógicos

---
+++ Dart-Basico/operadores-logicos.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  int n = 10;
+  int n2 = 13;
+  
+  bool r = n == 15;
+  bool r2 = n != 15;
+  bool r3 = n < n2;
+  bool r4 = n <= n2 && n >= 10;
+  bool r5 = n <= n2 || n >= 10;
+  
+  print(r);
+  print(r2);
+  print(r3);
+  print(r4);
+  print(r5);
+  
+}
