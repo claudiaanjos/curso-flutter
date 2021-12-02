39d0694b6fceae952ffef1d7d1ced045992b320c
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-02 13:24:52 -0300

    Variáveis Básicas

---
+++ Dart-Basico/variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  int idade = 35;
+  String nome = "José";
+  double distancia = 45.5;
+  
+  print(idade);
+  print(nome);
+  print(distancia);
+  
+  idade = 31;
+  print(idade);
+  
+}
