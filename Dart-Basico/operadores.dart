b33d4a4bc0aff2f3bfd8b9ca07445cb4713cb848
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-02 13:30:11 -0300

    Operadores Aritméticos

---
+++ Dart-Basico/operadores.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  double n1 = 10.5;
+  double n2 = 47.5;
+  int n3 = 3;
+  
+  double r1 = n1 * n2;
+  print(r1);
+  
+  double r2 = (n1 + n2) * n3;
+  print(r2);
+  
+  n3 = n3 + 10; // same: n3 += 10;
+  print(n3);
+  
+  n3++;
+  print(n3);
+  
+  print(n3 % 2);
+  
+}
