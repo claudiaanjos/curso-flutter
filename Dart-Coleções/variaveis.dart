0bdf71d8217a23799198b58fc55b9a127a40dedc
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-08 10:24:48 -0300

    Variáveis Dinâmicas

---
+++ Dart-Coleções/variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  var nome = "Daniel";
+  nome = "José";
+  //se colocar nome = 3 dá erro, pois o tipo é int (não pode mudar o tipo)
+  
+  var idade = 40;
+  idade++;
+  
+  dynamic variavel = "Daniel";
+  variavel = 3;
+  variavel = true;
+  
+  //usar o dynamic em situações especifícas
+}
