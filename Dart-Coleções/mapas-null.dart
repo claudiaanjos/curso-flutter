3d4a0cb01b514f87bfcc66e92fe07b94ff3108b7
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-08 11:16:02 -0300

    Mapas no Null-Safety

---
+++ Dart-Coleções/mapas-null.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  Map<int, String?> dds = {
+   11: "SP",
+   19: "Campinas",
+   41: "Curitiba"
+ };
+  
+  Map<int, String>? dds2 = {
+   11: "SP",
+   19: "Campinas",
+   41: "Curitiba"
+ };
+  
+  Map<int, String?>? dds3 = {
+   11: "SP",
+   19: "Campinas",
+   41: "Curitiba"
+ };
+  
+}
