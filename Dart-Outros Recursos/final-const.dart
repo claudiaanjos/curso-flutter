d7891d1ec7d1782b6ebdce61bf3837fc74f91514
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-12-13 11:11:57 -0300

    Final x Const

---
+++ Dart-Outros Recursos/final-const.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  const String nome = 'Daniel';
+// nome = "José"; erro, pois não posso mudar uma const
+  const List<String> lista = ['José', 'Maria'];
+  // não precisa especificar o tipo
+  const lista2 = ['José', 'Maria'];
+  
+  final String sobrenome = 'Costa';
+  
+  final Datetime agora = DateTime.now();
+  //não posso trocar a variável
+  //final = para quando temos valores atribuidos a tempo de execução
+}
